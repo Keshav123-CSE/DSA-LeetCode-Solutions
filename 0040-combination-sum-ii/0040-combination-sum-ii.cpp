72a416c8d4ff7295e5d91d36966889445bd63b63
Keshav Agrawal <Keshavagrawal9443@gmail.com>
2026-07-05 13:56:11 +0530

    Time: 3 ms (38.04%), Space: 14.1 MB (38.41%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -18,7 +18,7 @@ private:
 
         for (int i = start; i < candidates.size(); i++) {
             
-            if (candidates[i] > target) break;
+           if (candidates[i] > target) break;
 
            
             if (i > start && candidates[i] == candidates[i - 1]) continue;
