3c4adf8563c3f50387a475c693cbec745644db76
Keshav Agrawal <Keshavagrawal9443@gmail.com>
2026-07-08 00:03:07 +0530

    Time: 2 ms (40.25%), Space: 10.4 MB (67.67%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,7 +3,7 @@ public:
     void solve(int index, vector<int>& nums, vector<int>& currentsums, vector<vector<int>>& result) {
         result.push_back(currentsums);
         for (int i = index; i < nums.size(); i++) {
-            if (i > index && nums[i] == nums[i - 1]) continue; // skip duplicates at same level
+            if (i > index && nums[i] == nums[i - 1]) continue; 
             currentsums.push_back(nums[i]);
             solve(i + 1, nums, currentsums, result);
             currentsums.pop_back();
@@ -11,7 +11,7 @@ public:
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); // sorting is essential for the duplicate-skip trick to work
+        sort(nums.begin(), nums.end()); 
         vector<vector<int>> result;
         vector<int> currentsums;
         solve(0, nums, currentsums, result);
