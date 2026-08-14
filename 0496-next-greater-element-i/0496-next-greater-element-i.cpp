4a588ce380b6e9ed59ae95cac74bfb53c63c9f68
Keshav Agrawal <Keshavagrawal9443@gmail.com>
2026-08-14 18:11:02 +0530

    Time: 0 ms (100%), Space: 12.9 MB (15.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> nge;
+
+        for (int num : nums2) {
+            while (!st.empty() && st.top() < num) {
+                nge[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+        while (!st.empty()) {
+            nge[st.top()] = -1;
+            st.pop();
+        }
+
         vector<int> ans;
         for (int x : nums1) {
-            int idx = -1;
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums2[j] == x) { idx = j; break; }
-            }
-            int result = -1;
-            for (int j = idx + 1; j < nums2.size(); j++) {
-                if (nums2[j] > x) { result = nums2[j]; break; }
-            }
-            ans.push_back(result);
+            ans.push_back(nge[x]);
         }
         return ans;
     }
