76bbb5e8654d4c37aefd6e44ed80a3dbe7a3b602
Keshav Agrawal <Keshavagrawal9443@gmail.com>
2026-08-23 10:52:47 +0530

    Time: 119 ms (5.09%), Space: 27.7 MB (99.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& panda) {
-        int n = panda.size();
-        vector<int> result(n, -1);
-        stack<int> st;
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
 
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            
-            while (!st.empty() && st.top() <= panda[i % n]) {
-                st.pop();
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 1; j < n; j++) {
+
+                int index = (i + j) % n;
+
+                if (nums[index] > nums[i]) {
+                    ans[i] = nums[index];
+                    break;
+                }
             }
-
-
-            if (i < n) {
-                result[i] = st.empty() ? -1 : st.top();
-            }
-
-          
-            st.push(panda[i % n]);
         }
 
-        return result;
+        return ans;
     }
 };
